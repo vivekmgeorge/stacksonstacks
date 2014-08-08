43637af74a5705db8e8e79aec424b83a63a39c91
Dylan Richards <drichards2013@gmail.com>
2014-08-08 15:34:02 -0400

    ran AddUserIdToCommetables migration to create user associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808190220) do
+ActiveRecord::Schema.define(version: 20140808192519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140808190220) do
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: true do |t|
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140808190220) do
     t.string   "commentable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "questions", force: true do |t|
@@ -36,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140808190220) do
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
