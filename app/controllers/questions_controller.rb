f7c5554c1dd7dfaa7b8ad88cfb347bd65070cbd4
Dylan Richards <drichards2013@gmail.com>
2014-08-08 16:37:49 -0400

    display answers on question show view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,28 @@ class QuestionsController < ApplicationController
   end
 
   def show
+  	@question = Question.find(params[:id])
+  end
+
+  def new
+  	@question = Question.new
+  end
+
+  def edit
+  	
+  end
+
+  def create
+  	@question = Question.new(question_params)
+  end
+
+  private
+
+  def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+  	params.require(:question).permit(:title, :body)
   end
 end
