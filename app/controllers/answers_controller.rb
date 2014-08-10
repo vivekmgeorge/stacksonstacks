87a4980146c50b90d4f0104114e37f741a4ce0bc
Michael <immichaelweiss@gmail.com>
2014-08-09 20:13:19 -0400

    Add create, index and destroy methods to answer model

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,21 @@
 class AnswersController < ApplicationController
+	def index
+		@answers = Answer.all
+	end
+
 	def new
 		@answer = Answer.new
+		@question = Question.find(params[:question_id])
 	end
 
 	def create
-		@answer = Answer.create(params[:id])
-		@answer.question = @question
-		redirect_to '/questions/'
+		@question = find_question
+		@answer = @question.answers.build(answer_params)
+		if @answer.save
+			redirect_to question_path(@question)
+		else
+			render :action => 'new'
+		end
 	end
 
 	def edit
@@ -17,9 +26,38 @@ class AnswersController < ApplicationController
 		@answer = Answer.find(params[:id])
 	end
 
+	def destroy 
+		@answer = Answer.find(params[:id])
+		@answer.destroy
+	end
+
+  # def upvote
+	 #  @question = Question.find(params[:question_id])
+  #   @answer = Answer.find(params[:id])
+  #   @answer.upvote_from current_user
+  #   redirect_to question_path(@question)
+  # end
+
+  # def downvote
+  # 	@question = Question.find(params[:question_id])
+  #   @answer = Answer.find(params[:id])
+  #   @answer.downvote_from current_user
+  #   redirect_to question_path(@question)
+  # end
+
 	private
 
 	def answer_params
 		params.require(:answer).permit(:body, :question_id)
 	end
+
+	def find_question
+		params.each do |name, value|
+			if name =~ /(.+)_id$/
+				return $1.classify.constantize.find(value)
+			end
+		end
+		nil
+	end
+
 end
