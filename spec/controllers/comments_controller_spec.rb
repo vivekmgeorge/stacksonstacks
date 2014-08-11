53fc662c7be1b8dcb57621c65cbe0bf60d159bc8
David Sin <davidsin1990@gmail.com>
2014-08-10 22:41:15 -0400

    began a few tests for controllers

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,25 @@
+# require 'spec_helper'
+
+# describe CommentsController do
+
+#   describe '#index' do
+#     it 'renders the answer page' do
+#       get :index
+#       expect(response).to render_template :index
+#     end
+#   end
+
+#   describe '#new' do
+#     it 'renders the new page' do
+#       get :new
+#       expect(response).to render_template :new
+#     end
+
+#     it 'assigns a new Answer to @question' do
+#       get :new
+#       expect(assigns(:question)).to be_an_instance_of(Answer)
+#     end
+#   end
+
+
+# end
