d713bb062601739be761eb97fdbb495f04cf8679
David Sin <davidsin1990@gmail.com>
2014-08-10 22:41:42 -0400

    cannot pull till i commit these files

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,8 +1,8 @@
-require 'spec_helper' 
+require 'spec_helper'
 
 describe QuestionsController do
 
-	describe '#index' do 
+	describe '#index' do
 		it 'renders the index page' do
 			get :index
 			expect(response).to render_template :index
@@ -15,11 +15,26 @@ describe QuestionsController do
 			expect(response).to render_template :new
 		end
 
+		it 'creates a new instance' do
+			expect(assigns(:question)).to eq(Question.last)
+		end
 		it 'assigns a new Question to @question' do
 			get :new
 			expect(assigns(:question)).to be_an_instance_of(Question)
 		end
 	end
 
+	describe '#create' do
+ 		it "creates a question with valid params" do
+      get :create, { question: { title: "hi david", body: "OMG" }}
+      expect(assigns(:question)).to eq(Question.last)
+    end
+  end
 
+	context "#destroy" do
+  	it "#deletes post" do
+      question = Question.create!(title: "no more dishes", content: "boo!")
+      expect{ delete :destroy, id: question.id }.to change(Question, :count).by(-1)
+    end
+  end
 end
